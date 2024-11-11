073883e4d079e7c005d3f82593a2e4318d042cd1
ankitsinghmindfire <ankit.singh@mindfiresolutions.com>
2024-11-11 10:56:15 +0530

    removed commented code

--- client/src/utils/appConstants.js
+++ client/src/utils/appConstants.js
@@ -1,6 +1,8 @@
 export const passwordRegex =
   /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/;
+
 export const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
 export const nameRegex = /^[A-Z][a-zA-Z0-9-' ]{1,}$/;
 
 export const ratingData = [
