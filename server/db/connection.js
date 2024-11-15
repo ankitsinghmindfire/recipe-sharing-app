729d3220e2278905f2a01d7ac1bf5f9c7eb7cdf3
ankitsinghmindfire <ankit.singh@mindfiresolutions.com>
2024-11-15 12:50:40 +0530

    Fixing api issue

--- server/db/connection.js
+++ server/db/connection.js
@@ -4,7 +4,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 mongoose
-  .connect(process.env.URI)
+  .connect(process.env.URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("Connected to the database");
   })
